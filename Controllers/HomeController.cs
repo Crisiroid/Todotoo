59dc72b8e8545811fd6bcc38df3285da5cd003fa
Crisiroid <csiroid@gmail.com>
2023-08-07 00:21:55 +0430

    Register, user Management and Edit Functionality added

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using todotoo.Data;
 
 namespace todotoo.Controllers
 {
     public class HomeController : Controller
     {
+        private todotooContext db = new todotooContext();
         public ActionResult Index()
         {
             return View();
@@ -16,6 +18,24 @@ namespace todotoo.Controllers
         public ActionResult About()
         {
             return View();
+        }
+        public ActionResult Login(string Username, string Password)
+        {
+            if(CheckInformation(Username, Password))
+            {
+                ViewBag.pm = "Login SuccessFull";
+                return RedirectToAction("Index", "Home");
+            }
+            else {
+                ViewBag.pm = "Login Failed!";
+                return RedirectToAction("Index", "Home"); }
+            
+        }
+
+        public bool CheckInformation(string Username, String Password)
+        {
+            return db.Users.Any(u => u.Username == Username && u.Password == Password);
+
         }
     }
 }
---
+++ Controllers/UsersController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using todotoo.Data;
+using todotoo.Models;
+
+namespace todotoo.Controllers
+{
+    public class UsersController : Controller
+    {
+        private todotooContext db = new todotooContext();
+
+        public ActionResult Index()
+        {
+            return View(db.Users.ToList());
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "UserID,FullName,Username,Password,Email,LastActiveDate")] User user)
+        {
+            user.LastActiveDate = DateTime.Now.ToString();
+            if (ModelState.IsValid)
+            {
+                var id = db.Users.Max(item => item.UserID);
+                var tableName = "UserTodo" + id.ToString();
+                db.Users.Add(user);
+                db.Database.ExecuteSqlCommand(
+                    $"CREATE TABLE {tableName} (ID INT PRIMARY KEY, Title NVARCHAR(100), Description NVARCHAR(200), Priority NVARCHAR(20), Status NVARCHAR(20))");
+                db.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(user);
+        }
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "UserID,FullName,Username,Password,Email,LastActiveDate")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(user).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(user);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            User user = db.Users.Find(id);
+            db.Users.Remove(user);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
