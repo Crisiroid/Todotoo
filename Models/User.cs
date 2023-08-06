3c5152824caf71b4f2e267318b27d2add5ff3a9f
Crisiroid <csiroid@gmail.com>
2023-08-06 17:44:05 +0430

    Project created

---
+++ Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace todotoo.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserID { get; set; }
+        [Required]
+        public string FullName { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string LastActiveDate { get; set; }
+    }
+}
